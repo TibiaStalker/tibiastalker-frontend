47d30fb54ae5e671465a2cb16fdd55a9779b1be4
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-07-15 17:33:36 +0200

    add hook for character tracking support

---
+++ src/features/Tracking/hook/useCharacterTracking.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useMemo, useSyncExternalStore } from "react";
+
+import { CharacterStatus } from "../enums";
+import { charactersTrackingStore } from "../store/charactersTrackingStore";
+
+export const useCharacterTracking = (characterName: string) => {
+  const characterTrackingMap = useSyncExternalStore(charactersTrackingStore.subscribe, charactersTrackingStore.getSnapshot);
+
+  const onlineStatus = useMemo(() => {
+    const characterTrackingInfo = characterTrackingMap.get(characterName);
+
+    if (!characterTrackingInfo || characterTrackingInfo.isOnline === undefined) {
+      return CharacterStatus.unknown;
+    }
+
+    return characterTrackingInfo.isOnline ? CharacterStatus.online : CharacterStatus.offline;
+  }, [characterTrackingMap, characterName]);
+
+  const track = useCallback(async () => {
+    charactersTrackingStore.addCharacterToTracking(characterName);
+  }, []);
+
+  const untrack = useCallback(async () => {
+    charactersTrackingStore.removeCharacterFromTracking(characterName);
+  }, []);
+
+  return { track, untrack, onlineStatus };
+};
---
+++ src/features/Tracking/store/charactersTrackingStore.ts
@@ -0,0 +1,57 @@
+import * as liveCommunication from "../../../utils/liveCommunication";
+
+let actualCharacterStatusMap = new Map();
+let listeners = [];
+
+const emitChange = () => {
+  for (const listener of listeners) {
+    listener();
+  }
+};
+
+liveCommunication.connection.on("Character Tracker", event => {
+  const newCharacterStatusMap = new Map(actualCharacterStatusMap);
+
+  newCharacterStatusMap.set(event.name, event);
+  actualCharacterStatusMap = newCharacterStatusMap;
+  emitChange();
+});
+
+export const charactersTrackingStore = {
+  addCharacterToTracking: async (characterName: string) => {
+    if (actualCharacterStatusMap.has(characterName)) {
+      return;
+    }
+
+    const newCharacterStatusMap = new Map(actualCharacterStatusMap);
+    newCharacterStatusMap.set(characterName, { isOnline: undefined });
+
+    await liveCommunication.start();
+
+    liveCommunication.connection.send("JoinGroup", characterName);
+
+    actualCharacterStatusMap = newCharacterStatusMap;
+    emitChange();
+  },
+  removeCharacterFromTracking: async (characterName: string) => {
+    if (!actualCharacterStatusMap.has(characterName)) {
+      return;
+    }
+
+    await liveCommunication.connection.send("LeaveGroup", characterName);
+    const newCharacterStatusMap = new Map(actualCharacterStatusMap);
+    newCharacterStatusMap.delete(characterName);
+
+    actualCharacterStatusMap = newCharacterStatusMap;
+    emitChange();
+  },
+  subscribe: listener => {
+    listeners = [...listeners, listener];
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  },
+  getSnapshot: () => {
+    return actualCharacterStatusMap;
+  },
+};
