565ad65048d4583df9b7f2eff1667d8d199e8fda
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-07-16 21:10:29 +0200

    quick fix problem with typescript and the theme types

--- src/components/ApplicationBar/styles.tsx
+++ src/components/ApplicationBar/styles.tsx
@@ -2,7 +2,9 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled("div")(({ theme }) => ({
   paddingTop: "6rem",
-
+  // TODO: better fix this
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   [theme.breakpoints.up("sm")]: {
     paddingTop: "7rem",
   },
