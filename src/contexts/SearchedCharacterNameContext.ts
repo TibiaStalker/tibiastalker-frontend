ccc85ad762819eadb1cc806d0020aeb671b1ce66
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-07-17 17:15:37 +0200

    replace old way to search character

--- src/contexts/SearchedCharacterNameContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-import StartSearchingCharacter from "../types/StartSearchingCharacter";
-
-export const SearchedCharacterNameContext = createContext<StartSearchingCharacter>(() => {
-  return;
-});
