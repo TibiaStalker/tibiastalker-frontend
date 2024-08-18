0cff618fd35d05e01d86d8ee3e8a8d5768722272
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2024-08-18 13:56:34 +0200

    polished character autocomplete input

--- src/features/CharacterSearch/components/CharacterAutocompleteInput/CharacterAutocompleteInput.tsx
+++ src/features/CharacterSearch/components/CharacterAutocompleteInput/CharacterAutocompleteInput.tsx
@@ -1,71 +1,56 @@
 import Autocomplete from "@mui/material/Autocomplete";
-import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 import usePromptList from "../../hooks/usePromptList";
 import { CharacterAutocompleteInputProps } from "./types";
 
 export const CharacterAutocompleteInput = ({
-  children,
   placeholder,
   ariaLabel,
-  onSubmit,
-  clearAfterSubmit = false,
+  name,
+  onSelected,
+  value,
+  acceptEnteredValue = false,
 }: CharacterAutocompleteInputProps) => {
-  const { promptList, getPromptList, clearPromptList } = usePromptList();
-  const [localValue, setLocalValue] = useState("");
+  const { promptList, getPromptList } = usePromptList();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFocusOnInput, setIsFocusOnInput] = useState(false);
 
-  const updateInputValue = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const characterName = event.target.value;
-
-    setLocalValue(characterName);
-    getPromptList(characterName);
-  };
-
-  const selectedFromPrompt = (_: never, newValue: string) => {
-    if (clearAfterSubmit) {
-      setLocalValue("");
-    } else {
-      setLocalValue(newValue);
-    }
-
-    if (onSubmit) {
-      onSubmit(newValue);
-    }
-
-    clearPromptList();
+  const getPrompts = (event: never, characterName: string) => {
+    isFocusOnInput && getPromptList(characterName);
   };
 
   return (
-    <Stack
-      direction={{ xs: "column", sm: "row" }}
-      alignSelf="center"
-      spacing={1}
-      useFlexGap
-      sx={{ pt: 2, width: { xs: "100%", sm: "auto" } }}
-    >
-      <Autocomplete
-        freeSolo
-        options={promptList}
-        filterOptions={v => v}
-        value={localValue}
-        onChange={selectedFromPrompt}
-        renderInput={params => (
-          <TextField
-            {...params}
-            hiddenLabel
-            size="small"
-            variant="outlined"
-            aria-label={ariaLabel}
-            placeholder={placeholder}
-            onChange={updateInputValue}
-          />
-        )}
-      />
-      {children}
-    </Stack>
+    <Autocomplete
+      freeSolo={acceptEnteredValue}
+      options={promptList}
+      filterOptions={v => v}
+      onInputChange={getPrompts}
+      onFocus={() => setIsFocusOnInput(true)}
+      onBlur={() => setIsFocusOnInput(false)}
+      open={isOpen && promptList.length > 0}
+      onOpen={() => {
+        setIsOpen(true);
+      }}
+      onClose={() => {
+        setIsOpen(false);
+      }}
+      value={value}
+      onChange={(event: never, value: string | null) =>
+        onSelected && onSelected(value)
+      }
+      renderInput={params => (
+        <TextField
+          {...params}
+          hiddenLabel
+          name={name}
+          size="small"
+          variant="outlined"
+          aria-label={ariaLabel}
+          placeholder={placeholder}
+        />
+      )}
+    />
   );
 };
